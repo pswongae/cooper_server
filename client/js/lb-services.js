4d67acafdafbffde1fe790c29b272924ba9cf508
Pak Sum Wong <Pak Sum Wong>
2015-12-13 20:26:11 +0800

    updated remote method createPost

--- client/js/lb-services.js
+++ client/js/lb-services.js
@@ -4553,6 +4553,45 @@ module.factory(
           method: "POST"
         },
 
+        /**
+         * @ngdoc method
+         * @name lbServices.Post#getPost
+         * @methodOf lbServices.Post
+         *
+         * @description
+         *
+         * <em>
+         * (The remote method definition does not provide any description.)
+         * </em>
+         *
+         * @param {Object=} parameters Request parameters.
+         *
+         *   This method does not accept any parameters.
+         *   Supply an empty object or omit this argument altogether.
+         *
+         * @param {Object} postData Request data.
+         *
+         * This method expects a subset of model properties as request parameters.
+         *
+         * @param {function(Object,Object)=} successCb
+         *   Success callback with two arguments: `value`, `responseHeaders`.
+         *
+         * @param {function(Object)=} errorCb Error callback with one argument:
+         *   `httpResponse`.
+         *
+         * @returns {Object} An empty reference that will be
+         *   populated with the actual data once the response is returned
+         *   from the server.
+         *
+         * Data properties:
+         *
+         *  - `post` – `{object=}` - 
+         */
+        "getPost": {
+          url: urlBase + "/posts/getPost",
+          method: "POST"
+        },
+
         // INTERNAL. Use Member.post.findById() instead.
         "::findById::member::post": {
           params: {

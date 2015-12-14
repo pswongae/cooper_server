f621b356a017122d13f2dbdc43d934d226d1adb3
Pak Sum Wong <Pak Sum Wong>
2015-12-14 21:56:46 +0800

    DONE: createPosition, getPosition, applyPosition

--- client/js/lb-services.js
+++ client/js/lb-services.js
@@ -9829,6 +9829,123 @@ module.factory(
           method: "POST"
         },
 
+        /**
+         * @ngdoc method
+         * @name lbServices.Position#createPosition
+         * @methodOf lbServices.Position
+         *
+         * @description
+         *
+         * <em>
+         * (The remote method definition does not provide any description.)
+         * </em>
+         *
+         * @param {Object=} parameters Request parameters.
+         *
+         *   This method does not accept any parameters.
+         *   Supply an empty object or omit this argument altogether.
+         *
+         * @param {Object} postData Request data.
+         *
+         * This method expects a subset of model properties as request parameters.
+         *
+         * @param {function(Object,Object)=} successCb
+         *   Success callback with two arguments: `value`, `responseHeaders`.
+         *
+         * @param {function(Object)=} errorCb Error callback with one argument:
+         *   `httpResponse`.
+         *
+         * @returns {Object} An empty reference that will be
+         *   populated with the actual data once the response is returned
+         *   from the server.
+         *
+         * Data properties:
+         *
+         *  - `position` – `{object=}` - 
+         */
+        "createPosition": {
+          url: urlBase + "/positions/createPosition",
+          method: "POST"
+        },
+
+        /**
+         * @ngdoc method
+         * @name lbServices.Position#applyPosition
+         * @methodOf lbServices.Position
+         *
+         * @description
+         *
+         * <em>
+         * (The remote method definition does not provide any description.)
+         * </em>
+         *
+         * @param {Object=} parameters Request parameters.
+         *
+         *   This method does not accept any parameters.
+         *   Supply an empty object or omit this argument altogether.
+         *
+         * @param {Object} postData Request data.
+         *
+         * This method expects a subset of model properties as request parameters.
+         *
+         * @param {function(Object,Object)=} successCb
+         *   Success callback with two arguments: `value`, `responseHeaders`.
+         *
+         * @param {function(Object)=} errorCb Error callback with one argument:
+         *   `httpResponse`.
+         *
+         * @returns {Object} An empty reference that will be
+         *   populated with the actual data once the response is returned
+         *   from the server.
+         *
+         * Data properties:
+         *
+         *  - `position` – `{object=}` - 
+         */
+        "applyPosition": {
+          url: urlBase + "/positions/applyPosition",
+          method: "POST"
+        },
+
+        /**
+         * @ngdoc method
+         * @name lbServices.Position#getPosition
+         * @methodOf lbServices.Position
+         *
+         * @description
+         *
+         * <em>
+         * (The remote method definition does not provide any description.)
+         * </em>
+         *
+         * @param {Object=} parameters Request parameters.
+         *
+         *   This method does not accept any parameters.
+         *   Supply an empty object or omit this argument altogether.
+         *
+         * @param {Object} postData Request data.
+         *
+         * This method expects a subset of model properties as request parameters.
+         *
+         * @param {function(Object,Object)=} successCb
+         *   Success callback with two arguments: `value`, `responseHeaders`.
+         *
+         * @param {function(Object)=} errorCb Error callback with one argument:
+         *   `httpResponse`.
+         *
+         * @returns {Object} An empty reference that will be
+         *   populated with the actual data once the response is returned
+         *   from the server.
+         *
+         * Data properties:
+         *
+         *  - `position` – `{object=}` - 
+         */
+        "getPosition": {
+          url: urlBase + "/positions/getPosition",
+          method: "POST"
+        },
+
         // INTERNAL. Use Member.position.findById() instead.
         "::findById::member::position": {
           params: {
--- common/models/position.js
+++ common/models/position.js
@@ -4,20 +4,136 @@ var loopback = require('loopback');
 
 module.exports = function(Position) {
 
-	// Position.createPosition = function(data, cb){
-	// 	var ctx = loopback.getCurrentContext();
-	// 	// var accessToken = ctx.get('accessToken');
-	// 	// console.log(accessToken);
-	// 	var currentUser = ctx && ctx.get('currentUser');
-	// 	// console.log(currentUser);
+	Position.createPosition = function(data, cb){
+		var positionArray = new Array();
+		Position.createPositionArray(positionArray, data, 0, function(err, positionArray){
+			if (err){
+				console.log(err);
+				cb(err, null);
+			} else{
+				cb(null, positionArray);
+			}
+		});
+	}
 
+	Position.createPositionArray = function(positionArray, data, i, cb){
+		if (i < data.position.length){
+			var positionData = data.position[i];
+			positionData.postId = data.postId;
+			Position.create(positionData, function(err, position){
+				if (err){
+					console.log(err);
+					cb(err, null);
+				} else{
+					console.log("Position created: ", position);
+					positionArray.push(position);
+					Position.createPositionArray(positionArray, data, i+1, cb);
+				}
+			});
+		} else{
+			cb(null, positionArray);
+		}
+	}
 
-	// }
+	Position.applyPosition = function(data, cb){
+		var ctx = loopback.getCurrentContext();
+		// var accessToken = ctx.get('accessToken');
+		// console.log(accessToken);
+    	var currentUser = ctx && ctx.get('currentUser');
+    	// console.log(currentUser);
 
-	// Position.remoteMethod('createPosition', {
-	// 	http: {path: '/createPosition', verb: 'post'},
-	// 	accepts: {arg: 'data', type: 'object', http:{source:'body'}},
-	// 	returns: {arg: 'position', type: 'object'}
-	// });
+    	Position.findById(data.positionId, function(err, position){
+    		if (err){
+    			console.log(err);
+    			cb(err, null);
+    		} else{
+    			if (position.memberId == null){
+	    			var positionData = {};
+	    			positionData.memberId = currentUser.id;
+	    			position.updateAttributes(positionData, function(err, position){
+	    				if (err){
+	    					console.log(err);
+	    					cb(err, null);
+	    				} else{
+	    					position.member(function(err, member){
+	    						if (err){
+	    							console.log(err);
+	    							cb(err, null);
+	    						} else{
+	    							position.applicant = member.username;
+	    							console.log("Position Applied: ", position);
+	    							cb(null, position);
+	    						}
+	    					});
+	    				}
+	    			});
+	    		} else{
+	    			console.log("Fail to Apply Position: ", position);
+	    			cb(null, null);
+	    		}
+    		}
+    	});
+	}
+
+	Position.getPosition = function(data, cb){
+		Position.find(data, function(err, position){
+			if (err){
+				console.log(err);
+				cb(err, null);
+			} else{
+				var positionArray = new Array();
+				Position.getPositionArray(positionArray, position, 0, function(err, positionArray){
+					if (err){
+						console.log(err);
+						cb(err, null);
+					} else{
+						console.log("Get Position(s): ", positionArray);
+						cb(null, positionArray);
+					}
+				});
+			}
+		});
+	}
+
+	Position.getPositionArray = function(positionArray, position, i, cb){
+		if (i < position.length){
+			var positionObj = position[i];
+			position[i].member(function(err, mem){
+				if (err){
+					console.log(err);
+					cb(err, null);
+				} else{
+					if (mem != null){
+						positionObj.applicant = mem.username;
+						positionObj.jobtitle = mem.jobtitle;
+						positionObj.description = mem.description;
+						positionObj.image = mem.image;
+					}
+					positionArray.push(positionObj);
+					Position.getPositionArray(positionArray, position, i+1, cb);
+				}
+			});
+		} else{
+			cb(null, positionArray);
+		}
+	}
+
+	Position.remoteMethod('createPosition', {
+		http: {path: '/createPosition', verb: 'post'},
+		accepts: {arg: 'data', type: 'object', http:{source:'body'}},
+		returns: {arg: 'position', type: 'object'}
+	});
+
+	Position.remoteMethod('applyPosition', {
+		http: {path: '/applyPosition', verb: 'post'},
+		accepts: {arg: 'data', type: 'object', http:{source:'body'}},
+		returns: {arg: 'position', type: 'object'}
+	});
+
+	Position.remoteMethod('getPosition', {
+		http: {path: '/getPosition', verb: 'post'},
+		accepts: {arg: 'data', type: 'object', http:{source:'body'}},
+		returns: {arg: 'position', type: 'object'}
+	});
 
 };
